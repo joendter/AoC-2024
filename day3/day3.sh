ca297ce3224cc7541f22206f04bbb64548e92a2f
joendter <71767955+joendter@users.noreply.github.com>
2024-12-03 11:13:49 +0100

    clean up code to work with any input

--- day3/day3.sh
+++ day3/day3.sh
@@ -1,29 +1,17 @@
-c_brackets="))))))))))"
-o_brackets="(((((((((("
-o100="$o_brackets$o_brackets$o_brackets$o_brackets$o_brackets$o_brackets$o_brackets$o_brackets$o_brackets$o_brackets"
 stuff=$(
     cat $1 |
     sed -E "s/\)/)\n/g" | # split everything to separate lines for grep to work properly
     grep -o -E "(mul\([0-9]{1,3},[0-9]{1,3}\))|(do(n't)?\(\))" | 
     sed -E "s/mul\(([0-9]*),([0-9]*)\)/\1*\2/" | 
     tr '\n' ' ' |
-    sed -E "s/do\(\)/$c_brackets/g" | 
-    sed -E "s/don't\(\)/0*(/g" |
-    sed -E "s/(.*)/print$o100$o100$o100$o_brackets$o_brackets$o_brackets$o_brackets$o_brackets$o_brackets(((((\1$c_brackets$c_brackets$c_brackets$c_brackets$c_brackets$c_brackets/" |
-    sed -E "s/([0-9)]) ([0-9])/\1+\2/g"
+    sed -E "s/do\(\)/ö/g" |
+    sed -E "s/don't\(\)[^ö]*?ö//g" |
+    sed -E "s/ö//g" |
+    sed -E "s/don't\(\).*$//g" |
+    sed -E "s/(.*)/print(\1)/" |
+    sed -E "s/([0-9)]) +([0-9])/\1+\2/g"
 )
 
-function my_test {
-    echo "$1" |
-        python3
-}
-
 echo "$stuff"
-echo "$stuff" |
-    sed -E "s/\(/(\n/g" |
-    wc -l
-echo "$stuff" |
-    sed -E "s/\)/)\n/g" |
-    wc -l
 echo "$stuff" | 
     python3
